8626698bbd4278e6a1f94c9ed9f62f0fc690db50
MSGuy01 <47047931+MSGuy01@users.noreply.github.com>
2022-04-05 20:19:14 -0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -20,17 +20,23 @@ public class Game {
             String in = s.next();
             switch(in) {
                 case "move":
-                    System.out.print("pos 1: ");
+                    System.out.print("current Y: ");
                     int pos1 = s.nextInt();
-                    System.out.print("pos 2: ");
+                    System.out.print("current X: ");
                     int pos2 = s.nextInt();
-                    System.out.print("Y: ");
+                    System.out.print("new Y: ");
                     int npos1 = s.nextInt();
-                    System.out.print("X: ");
+                    System.out.print("new X: ");
                     int npos2 = s.nextInt();
-                    Piece temp = board[pos1][pos2];
-                    board[pos1][pos2] = null;
-                    board[npos1][npos2] = temp;
+                    if (contains(checkMoves(pos1,pos2,1),new int[]{npos1,npos2})) {
+                        Piece temp = board[pos1][pos2];
+                        board[pos1][pos2] = null;
+                        board[npos1][npos2] = temp;
+                        System.out.println("MOVED!");
+                    }
+                    else {
+                        System.out.println("no");
+                    }
                     break;
                 case "checkMoves":
                     System.out.print("pos 1: ");
@@ -47,10 +53,122 @@ public class Game {
         }
     }
     public boolean checkSpace(int y, int x, int player) {
+        if (y < 0 || y > board.length || x < 0 || x > board.length) {
+            return false;
+        }
         return board[y][x] == null || (board[y][x].player != player && board[y][x].type != 5);
     }
+    public int[][] rook(int x,int y,int player,int index,int[][] arr) {
+        for (int i = y+1; i < 8; i++) {
+            try {
+                if (!checkSpace(i,x,player)) {
+                    break;
+                }
+                arr[index] = new int[]{i,x};
+                index++;
+            }
+            catch(ArrayIndexOutOfBoundsException a) {
+                break;
+            }
+        }
+        for (int i = y-1; i > 0; i--) {
+            try {
+                if (!checkSpace(i,x,player)) {
+                    break;
+                }
+                arr[index] = new int[]{i,x};
+                index++;
+            }
+            catch(ArrayIndexOutOfBoundsException a) {
+                break;
+            }
+        }
+        for (int i = x+1; i < 8; i++) {
+            try {
+                if (!checkSpace(y,i,player)) {
+                    break;
+                }
+                arr[index] = new int[]{y,i};
+                index++;
+            }
+            catch(ArrayIndexOutOfBoundsException a) {
+                break;
+            }
+        }
+        for (int i = x-1; i > 0; i--) {
+            try {
+                if (!checkSpace(y,i,player)) {
+                    break;
+                }
+                arr[index] = new int[]{y,i};
+                index++;
+            }
+            catch(ArrayIndexOutOfBoundsException a) {
+                break;
+            }
+        }
+        return arr;
+    }
+    public int[][] bishop(int x,int y,int player,int index, int[][] arr) {
+        int j = x+1;
+        for (int i = y+1; i < 8; i++) {
+            try {
+                if (!checkSpace(i,j,player)) {
+                    break;
+                }
+                arr[index] = new int[]{i,j};
+                index++;
+            }
+            catch(ArrayIndexOutOfBoundsException a) {
+                break;
+            }
+            j++;
+        }
+        j = x-1;
+        for (int i = y-1; i > 0; i--) {
+            try {
+                if (!checkSpace(i,x,player)) {
+                    break;
+                }
+                arr[index] = new int[]{i,j};
+                index++;
+            }
+            catch(ArrayIndexOutOfBoundsException a) {
+                break;
+            }
+        }
+        j = x+1;
+        for (int i = y-1; i > 0; i--) {
+            try {
+                if (!checkSpace(i,j,player)) {
+                    break;
+                }
+                arr[index] = new int[]{i,j};
+                index++;
+            }
+            catch(ArrayIndexOutOfBoundsException a) {
+                break;
+            }
+            j++;
+        }
+        j = x-1;
+        for (int i = y+1; i < 8; i++) {
+            try {
+                if (!checkSpace(i,j,player)) {
+                    break;
+                }
+                arr[index] = new int[]{i,j};
+                index++;
+            }
+            catch(ArrayIndexOutOfBoundsException a) {
+                break;
+            }
+            j++;
+        }
+        return arr;
+    }
     public int[][] checkMoves(int y, int x, int player) {
-        if (board[y][x] == null) {
+        if ((y < 0 || y > board.length || x < 0 || x > board.length) || (board[y][x] == null))  {
             return new int[][]{{-1}};
         }
         else {
@@ -65,86 +183,53 @@ public class Game {
                     break;
                 //rook
                 case 1:
-                    for (int i = y+1; i < 8; i++) {
-                        try {
-                            if (!checkSpace(i,x,player)) {
-                                break;
-                            }
-                            arr[index] = new int[]{i,x};
-                            index++;
-                        }
-                        catch(ArrayIndexOutOfBoundsException a) {
-                            break;
-                        }
-                    }
-                    System.out.println(1);
-                    for (int i = y-1; i > 0; i--) {
-                        try {
-                            if (!checkSpace(i,x,player)) {
-                                break;
-                            }
-                            arr[index] = new int[]{i,x};
-                            index++;
-                        }
-                        catch(ArrayIndexOutOfBoundsException a) {
-                            break;
-                        }
-                    }
-                    System.out.println(2);
-                    for (int i = x+1; i < 8; i++) {
-                        try {
-                            if (!checkSpace(y,i,player)) {
-                                break;
-                            }
-                            arr[index] = new int[]{y,i};
-                            index++;
-                        }
-                        catch(ArrayIndexOutOfBoundsException a) {
-                            break;
-                        }
-                    }
-                    System.out.println(3);
-                    for (int i = x-1; i > 0; i--) {
-                        try {
-                            if (!checkSpace(y,i,player)) {
-                                break;
-                            }
-                            arr[index] = new int[]{y,i};
-                            index++;
-                        }
-                        catch(ArrayIndexOutOfBoundsException a) {
-                            break;
-                        }
-                    }
+                    arr = rook(x,y,player,index,arr);
                     break;
                 //knight
                 case 2:
                     int[] poss = new int[]{1,-1,2,-2};
-                    for (int i = 0; i < poss.length; i++) {
-                        for (int j = 0; j < poss.length; j++) {
+                    for (int j : poss) {
+                        for (int k : poss) {
                             try {
-                                if (!checkSpace(y+j,x+i,player)) {
-                                    break;
+                                if (!checkSpace(y + k, x + j, player) || Math.abs(j) == Math.abs(k)) {
+                                    continue;
                                 }
-                                arr[index] = new int[]{y+j,x+i};
-                            }
-                            catch(ArrayIndexOutOfBoundsException a) {
-                                break;
+                                arr[index] = new int[]{y + k, x + j};
+                                index++;
+                            } catch (ArrayIndexOutOfBoundsException ignored) {
                             }
                         }
                     }
                     break;
                 //bishop
                 case 3:
-                    break;
-                //yueen
-                case 4:
+                    arr = bishop(x,y,player,index,arr);
                     break;
                 //king
+                case 4:
+                    int[] posss = new int[]{-1,0,1};
+                    for (int i : posss) {
+                        for (int k : posss) {
+                            if ((i == 0 && k == 0) || !checkSpace(y+i,x+k,player)) {
+                                continue;
+                            }
+                            arr[index] = new int[]{y+i,x+k};
+                            index++;
+                        }
+                    }
+                    break;
+                //queen
                 case 5:
+                    arr = bishop(x,y,player,index,arr);
+                    for (int i = 0; i < arr.length; i++) {
+                        if (arr[i][0] == -1) {
+                            index = i;
+                        }
+                    }
+                    arr = rook(x,y,player,index,arr);
                     break;
                 default:
-                    System.err.println("No such piece eyists.");
+                    System.err.println("No such piece exists.");
                     break;
             }
             return arr;
@@ -165,5 +250,13 @@ public class Game {
             }
         }
         return new int[]{-1,-1};
+    }
+    public boolean contains(int[][] arr, int[] coord) {
+        for (int[] i : arr) {
+            if (Arrays.equals(i,coord)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
