226b67c4de5dc39480db026ea150182e240de85f
MSGuy01 <47047931+MatthewRBevins@users.noreply.github.com>
2022-09-03 12:36:19 -0700

    More comments and some debugging

--- src/Game.java
+++ src/Game.java
@@ -16,6 +16,9 @@ public class Game{
         Scanner s = new Scanner(System.in);
         //Set current player to player one
         int currentPlayer = 1;
+        System.out.print("Type 1 for computer player and 2 for multiplayer: ");
+        boolean comp = s.nextInt() == 1;
+        System.out.println(comp);
         //While the game is still running
         while (true) {
             //Index for row numbers
@@ -30,7 +33,7 @@ public class Game{
             //Loop through all board rows
             for (Piece[] r : board) {
                 //Print row numbers
-                System.out.print(ri+"| ");
+                System.out.print(ri + "| ");
                 ri++;
                 //Loop through pieces in current row
                 for (Piece p : r) {
@@ -46,115 +49,137 @@ public class Game{
                 }
                 System.out.println();
             }
-            //Ask player for choice
-            System.out.print("PLAYER " + currentPlayer + " CHOICE (type 'choices' to see options): ");
 
-
-            /*
-             * COMPUTER PLAYER
-             */
-
-
-
-
-
-            String in = s.next();
-            switch(in) {
-                //Print all choices for player
-                case "choices":
-                    System.out.println("'move': move a given piece to a new space.");
-                    System.out.println("'checkMoves': shows you which places you can move a given piece.");
-                    System.out.println("'stop': stops the game.");
-                    System.out.println("'checkAttacking': shows you a list of the pieces a given piece is attacking.");
-                    System.out.println("'checkAttackers': shows you a list of what pieces are attacking a given piece.");
-                    Tools.enterToContinue();
-                    break;
-                case "move":
-                    //Ask player for position to move to
-                    System.out.print("CURRENT ROW: ");
-                    int row1 = s.nextInt();
-                    System.out.print("CURRENT COLUMN: ");
-                    int col1 = s.nextInt();
-                    System.out.print("NEW ROW: ");
-                    int newRow1 = s.nextInt();
-                    System.out.print("NEW COLUMN: ");
-                    int newCol1 = s.nextInt();
-                    //If the possible moves for the current piece includes the new position given
-                    if (Tools.int2DArrContains(checkMoves(row1,col1,currentPlayer),new int[]{newRow1,newCol1})) {
-                        Piece temp = board[row1][col1];
-                        board[row1][col1] = null;
-                        board[newRow1][newCol1] = temp;
-                        System.out.println("MOVED!");
-                        if (currentPlayer == 1) {
-                            currentPlayer = 2;
+            if (! comp || currentPlayer == 1) {
+                //Ask player for choice
+                System.out.print("PLAYER " + currentPlayer + " CHOICE (type 'choices' to see options): ");
+                String in = s.next();
+                switch (in) {
+                    //Print all choices for player
+                    case "choices":
+                        System.out.println("'move': move a given piece to a new space.");
+                        System.out.println("'checkMoves': shows you which places you can move a given piece.");
+                        System.out.println("'stop': stops the game.");
+                        System.out.println("'checkAttacking': shows you a list of the pieces a given piece is attacking.");
+                        System.out.println("'checkAttackers': shows you a list of what pieces are attacking a given piece.");
+                        Tools.enterToContinue();
+                        break;
+                    case "move":
+                        //Ask player for position to move to
+                        System.out.print("CURRENT ROW: ");
+                        int row1 = s.nextInt();
+                        System.out.print("CURRENT COLUMN: ");
+                        int col1 = s.nextInt();
+                        System.out.print("NEW ROW: ");
+                        int newRow1 = s.nextInt();
+                        System.out.print("NEW COLUMN: ");
+                        int newCol1 = s.nextInt();
+                        //If the possible moves for the current piece includes the new position given
+                        if (Tools.int2DArrContains(checkMoves(row1, col1, currentPlayer), new int[]{newRow1, newCol1})) {
+                            //Store the piece being moved
+                            Piece temp = board[row1][col1];
+                            //Remove the piece from its original position
+                            board[row1][col1] = null;
+                            //Place the piece in its new position
+                            board[newRow1][newCol1] = temp;
+                            System.out.println("MOVED!");
+                            //Switch current player
+                            if (currentPlayer == 1) {
+                                currentPlayer = 2;
+                            } else {
+                                currentPlayer = 1;
+                            }
                         }
+                        //If the given move is not possible
                         else {
-                            currentPlayer = 1;
+                            System.out.println("***ILLEGAL MOVE***");
                         }
-                    }
-                    else {
-                        System.out.println("***ILLEGAL MOVE***");
-                    }
-                    Tools.enterToContinue();
-                    break;
-                case "checkMoves":
-                    System.out.print("ROW: ");
-                    int row2 = s.nextInt();
-                    System.out.print("COLUMN: ");
-                    int col2 = s.nextInt();
-                    System.out.println(Arrays.deepToString(checkMoves(row2,col2,currentPlayer)));
-                    Tools.enterToContinue();
-                    break;
-                case "checkAttacking":
-                    System.out.print("ROW: ");
-                    int row3 = s.nextInt();
-                    System.out.print("COLUMN: ");
-                    int col3 = s.nextInt();
-                    System.out.println(attacking(row3,col3,board[row3][col3].player).length);
-                    for (Piece p : attacking(row3,col3,board[row3][col3].player)) {
-                        System.out.print(Arrays.toString(p.getPos()));
-                    }
-                    System.out.println();
-                    break;
-                case "checkAttackers":
-                    break;
-                case "stop":
-                    System.exit(69);
-                default:
-                    break;
+                        Tools.enterToContinue();
+                        break;
+                    case "checkMoves":
+                        System.out.print("ROW: ");
+                        int row2 = s.nextInt();
+                        System.out.print("COLUMN: ");
+                        int col2 = s.nextInt();
+                        System.out.println(Arrays.deepToString(checkMoves(row2, col2, currentPlayer)));
+                        Tools.enterToContinue();
+                        break;
+                    case "checkAttacking":
+                        System.out.print("ROW: ");
+                        int row3 = s.nextInt();
+                        System.out.print("COLUMN: ");
+                        int col3 = s.nextInt();
+                        System.out.println(attacking(row3, col3, board[row3][col3].player).length);
+                        //Print all pieces the given piece is attacking
+                        for (Piece p : attacking(row3, col3, board[row3][col3].player)) {
+                            System.out.print(Arrays.toString(p.getPos()));
+                        }
+                        System.out.println();
+                        break;
+                    case "checkAttackers":
+                        System.out.print("ROW: ");
+                        int row4 = s.nextInt();
+                        System.out.print("COLUMN: ");
+                        int col4 = s.nextInt();
+                        System.out.println("***********");
+                        System.out.println(spaceUnderAttack(row4,col4,currentPlayer));
+                        break;
+                    case "stop":
+                        System.exit(69);
+                    default:
+                        break;
+                }
+            }
+            else {
+                /*
+                 * COMPUTER PLAYER
+                 */
             }
         }
     }
+
+
+    public int[] minimax(Piece[][] currentBoard, int depth, int player, int originalDepth) {
+        int[] best;
+        //computer player
+        if (player == 2) {
+            best = new int[]{-1,-1,-1000};
+        }
+        else {
+            best = new int[]{-1,-1,1000};
+        }
+        if (depth <= originalDepth - 5) { //OR if game over
+
+        }
+        return null;
+    }
+
     //PLAYER BEING ATTACKED
     public static boolean spaceUnderAttack(int y, int x, int player) {
-        if (board[y][x] == null || board[y][x].player != player) {
-            return false;
-        }
-        for (Piece[] pa : board) {
-            for (Piece p : pa) {
-                if (p.player != player && Tools.pieceArrContains(attacking(p.getPos()[0],p.getPos()[1],p.player),board[y][x])) {
-                    return true;
-                }
-            }
-        }
+
         return false;
     }
     public static Piece[] attacking(int y, int x, int player) {
         Piece[] f = new Piece[10];
         int[][] cm = checkMoves(y,x,player);
         int index = 0;
+        //Loop through all possible moves by a piece and see if it includes an opponent's piece
         for (int[] i : cm) {
-            if (board[i[0]][i[1]] != null) {
-                f[index] = board[i[0]][i[1]];
-                index++;
+            if (i[0] > -1 && i[1] > -1) {
+                if (board[i[0]][i[1]] != null) {
+                    f[index] = board[i[0]][i[1]];
+                    index++;
+                }
             }
         }
+        //Return list of pieces that the given piece is attacking
         Piece[] finalArr = new Piece[index];
         System.arraycopy(f, 0, finalArr, 0, finalArr.length);
         return finalArr;
     }
     public static boolean checkSpace(int y, int x, int player) {
-        if (y < 0 || y > board.length || x < 0 || x > board.length) {
+        //Check is a space is valid
+        if (y < 0 || y >= board.length || x < 0 || x >= board.length) {
             return false;
         }
         return board[y][x] == null || (board[y][x].player != player && board[y][x].type != 5);
@@ -310,8 +335,8 @@ public class Game{
                     break;
                 //rook
                 case 1:
-                    System.out.println("*****ROOK****");
-                    System.out.println(Arrays.deepToString(rook(x, y, player, index, arr)));
+                    //System.out.println("*****ROOK****");
+                    //System.out.println(Arrays.deepToString(rook(x, y, player, index, arr)));
                     arr = rook(x,y,player,index,arr).clone();
                     for (int i = 0; i < arr.length; i++) {
                         if (arr[i][0] == -1) {
@@ -319,7 +344,7 @@ public class Game{
                             break;
                         }
                     }
-                    System.out.println(Arrays.deepToString(arr));
+                    //System.out.println(Arrays.deepToString(arr));
                     break;
                 //knight
                 case 2:
@@ -372,7 +397,7 @@ public class Game{
             for (int i = 0; i < finalArr.length; i++) {
                 finalArr[i] = arr[i];
             }
-            System.out.println(Arrays.deepToString(finalArr));
+            //System.out.println(Arrays.deepToString(finalArr));
             return finalArr;
         }
     }
